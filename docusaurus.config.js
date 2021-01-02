be6ab318c6fa70d446c051a7d10043b947f4ea90
Sam <sam@developersam.com>
2021-01-02 10:27:37 -0500

    Setup docusaurus website based on markdown documents

---
+++ docusaurus.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  title: "Computerization Sponsorship",
+  tagline: "Website for Computerization Sponsorship agreement docs",
+  url: "https://computerization-sponsorship.developersam.com",
+  baseUrl: "/",
+  favicon: "https://developersam.com/favicon.ico",
+  themeConfig: require("./theme-config.json"),
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.json"),
+          editUrl: "https://github.com/sam-foundation/computerization-sponsorship/edit/master/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
--- style.rb
+++ style.rb
@@ -2,3 +2,5 @@
 all
 rule 'MD013', :line_length => 100
 rule 'MD029', :style => 'ordered'
+exclude_rule  'MD002'
+exclude_rule  'MD041'
